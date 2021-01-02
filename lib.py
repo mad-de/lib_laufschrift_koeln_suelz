da8ec663c963e107da1a43f8ea6fc2aca59d8af8
mad-de <cev_madde@msn.com>
2021-01-02 17:17:16 +0100

    Update lib.py

--- lib.py
+++ lib.py
@@ -153,16 +153,15 @@ def return_command_array():
                 newsticker = newsticker + get_ticker.encode('utf-8')
 
     # Recipe
-"""
-    rezept_url = \
-        urlopen('https://www.rezepteplan.de/SpeisePlan/Zuf%C3%A4lligerSpeisePlan.aspx?34=1'
-                )
-    rezept_html = rezept_url.read()
-    temp_html = rezept_html.split('<br /><a RecipeId=', 1)[1]
-    rezept_id = temp_html.split(' NiceTitle=', 1)[0].rstrip('"')[1:]
-    temp_html = temp_html.split(' NiceTitle=', 1)[1]
-    rezept_name = temp_html.split(' onmouseover', 1)[0].rstrip('"')[1:]
-"""
+#    rezept_url = \
+#        urlopen('https://www.rezepteplan.de/SpeisePlan/Zuf%C3%A4lligerSpeisePlan.aspx?34=1'
+#                )
+#    rezept_html = rezept_url.read()
+#    temp_html = rezept_html.split('<br /><a RecipeId=', 1)[1]
+#    rezept_id = temp_html.split(' NiceTitle=', 1)[0].rstrip('"')[1:]
+#    temp_html = temp_html.split(' NiceTitle=', 1)[1]
+#    rezept_name = temp_html.split(' onmouseover', 1)[0].rstrip('"')[1:]
+
     # TEXTE
     # Ab hier die einzelnen Zeilen Ã€ndern / oder über "]" eine neue hinzufügen. Der Ticker aktualisiert sich nach jedem Merge automatisch.
     # Beispiel:
@@ -191,37 +190,12 @@ def return_command_array():
             + '\xc2\xb0C. Gef\xc3\xbchlte Temperatur: '
             + str(json_weather[u'main'][u'feels_like']).replace('-',
                 u'\u2212').encode('utf-8') + '\xc2\xb0C.'
-            ,
-        'Unterst\xc3\xbctzt eure lokalen Gesch\xc3\xa4fte in dieser Zeit. Ihr wollt etwas lesen oder braucht eine DVD? Bestellt doch zum Beispiel \xc3\xbcber der-andere-buchladen-koeln@t-online.de.'
-            ,
-        'Viele Restaurants hier im Viertel bieten einen Lieferdienst oder Abholservice z.B. Pizza e Vino (0221 94388111), abs1.de, merstonnzesamme.de oder palanta.de. Auf bringsl.com gibt es auch einen Fahrradlieferdienst für Feinkost.'
-            ,
-        'Auf der Suche nach guter Musik ohne Werbung oder Corona-Nachrichten: www.radioparadise.com'
-            ,
-	'Unterstützt lokale Läden indem ihr jetzt einen Gutschein kauft unter www.veedelsretter.koeln'
-	    ,
-        #'Langweilig und Lust auf einen guten Film? Die 10 besten Filme des 21. Jahrhunderts (lt. BBC): 10) No Country for Old Men von Joel Coen and Ethan Coen 9) A Separation von Asghar Farhadi 8) Yi Yi von Edward Yang 7) The Tree of Life von Terrence Malick 6) Eternal Sunshine of the Spotless Mind von Michel Gondry 5) Boyhood von Richard Linklater 4) Spirited Away von Hayao Miyazaki 3) There Will Be Blood von Paul Thomas Anderson 2) In the Mood for Love von Wong Kar-wai 1) Mulholland Drive von David Lynch ... Viel Spass beim Anschauen!'
-        #    ,
-        'Was soll man nur kochen? Wie w\xc3\xa4re es mit '
-            + rezept_name
-            + '? Rezept unter: https://www.rezepteplan.de/Rezepte/.rezept?0='
-             + rezept_id + ' Guten Appetit!'
-            ,
-        #'Wie w\xc3\xa4re es mit einer neuen Serie? Die 10 besten Serien aller Zeiten (lt. Cinemablend): 10) The Simpsons 9) Mad Men 8) I love Lucy 7) Saturday Night Live 6) The Sopranos 5) Seinfeld 4) Game of Thrones 3) The X-Files 2) Breaking bad 1) Friends ... Viel Spass beim Suchten!'
-        #    ,
-        'Danke an Alle, die den Laden hier am Laufen halten!'
-            ,
-	'ABS verteilt jeden Tag kostenlos eine Anzahl von Essen an Bedürftige im Gottesweg 135 ab 12 Uhr. '
-            ,
-	'Habt ihr noch medizinisches Material wie Einmalhandschuhe oder Atemschutzmasken übrig? Spendet sie doch an Gesundheitsdienstleister auf remedymatch.io'
-            ,
-	'Von uns getestete, kostenlose Spiele f\xc3\xbcr einen Remote-Spieleabend mit (ebenfalls isolierten) Freunden: Trickster (App), Drawful 2 (Steam). Viel Spass beim Ausprobieren!'
-            ,
-        'Ihr braucht aktuell jemanden, der f\xc3\xbcr euch einkauft? Ihr wollt Hilfe anbieten? Freiwillige Helfer findet man unter anderem auf machbarschaft.jetzt & colivery.de & nebenan.de/corona & die-einkaufshelden.de oder auch telefonisch: 040 299960980. Oder ihr schreibt uns per Whatsapp: +44 7537 1818 02. Wir helfen auch gerne!'
-            ,
-	'Eure Nachricht hier anzeigen lassen? https://github.com/mad-de/lib_laufschrift_koeln_suelz/ Oder wollt ihr uns etwas mitteilen? Whatsapp: +44 7537 181802'
-            ,
-        ]
+#            ,
+#        'Was soll man nur kochen? Wie w\xc3\xa4re es mit '
+#            + rezept_name
+#            + '? Rezept unter: https://www.rezepteplan.de/Rezepte/.rezept?0='
+#             + rezept_id + ' Guten Appetit!'
+            ,        ]
 
   #  random.shuffle(command_array)
 
