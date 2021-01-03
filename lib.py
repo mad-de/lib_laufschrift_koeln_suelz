011c08f4642ca3e951faedf0dffb596a34b014d3
mad-de <cev_madde@msn.com>
2021-01-03 15:05:07 +0100

    Update lib.py

--- lib.py
+++ lib.py
@@ -43,7 +43,7 @@ else:
 
 btc24h = btc24h + str("{:.2f}".format(data[u'data'][u'BTC'][u'quote'][u'USD'][u'percent_change_24h']))
 
-crypto_ticker = "Die aktuellen Cryptopreise: Bitcoin: " + btcUSD + " $ (" + btcEUR + " {}); ".format(unicodedata.lookup("EURO SIGN") + btc24h + " % || Ethereum: " + ethUSD + " $ (" + ethEUR + " €); " + eth24h + " %"
+crypto_ticker = "Die aktuellen Cryptopreise: Bitcoin: " + btcUSD + " $ (" + btcEUR + " {}); ".format(unicodedata.lookup("EURO SIGN")) + btc24h + " % || Ethereum: " + ethUSD + " $ (" + ethEUR + " €); " + eth24h + " %"
 
 # CATFACTS
 
