1c677ddd6726cf456d88e51f6eb44b799f7fd498
mad-de <cev_madde@msn.com>
2021-01-04 12:51:43 +0100

    Update lib.py

--- lib.py
+++ lib.py
@@ -58,7 +58,7 @@ crypto_ticker = "Die aktuellen Cryptopreise: Bitcoin: " + btcUSD + " $ (" + btcE
 # Vaccinations
 
 import requests
-vaccination_url = requests.get('https://datawrapper.dwcdn.net/zbXpI/5/')
+vaccination_url = requests.get('https://datawrapper.dwcdn.net/zbXpI/6/')
 vaccination_html  = vaccination_url.text
 
 temp_html = vaccination_html.split('Germany,', 1)[1]
