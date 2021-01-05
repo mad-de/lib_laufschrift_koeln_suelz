f0b923067a459f62e4c6afd6aee6a588a8a973f4
mad-de <cev_madde@msn.com>
2021-01-05 23:08:40 +0100

    Update lib.py

--- lib.py
+++ lib.py
@@ -78,7 +78,7 @@ SG_stock_price = SG_stock_html.split('<span>', 1)[0]
 SG_stock_change = SG_stock_html.split('\">', 1)[1]
 SG_stock_change = SG_stock_change.split('<span>', 1)[0].replace('-', '- ').encode('utf-8')
 
-stock_ticker = "Die Aktienkurse: Meyer-Burger: " + MB_stock_price + " Euro (" + MB_stock_change + " %) || Siemens-Gamesa: " + SG_stock_price + " Euro (" + SG_stock_change + " %)"
+stock_ticker = "Die Aktienkurse: Meyer-Burger: " + str(format(MB_stock_price)) + " € (" + str(format(MB_stock_change)) + " %) || Siemens-Gamesa: " + str(format(SG_stock_price)) + " € (" + str(format(SG_stock_change)) + " %)"
 
 # Vaccinations
 
