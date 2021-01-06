e4643c05d64558e7dd2df8ba779c4269f82524d0
mad-de <cev_madde@msn.com>
2021-01-06 13:01:34 +0100

    Update lib.py

--- lib.py
+++ lib.py
@@ -81,11 +81,14 @@ SG_stock_change = SG_stock_change.split('<span>', 1)[0].replace('-', '- ').encod
 stock_ticker = "Die Aktienkurse: Meyer-Burger: " + str(format(MB_stock_price)) + " € (" + str(format(MB_stock_change)) + " %) || Siemens-Gamesa: " + str(format(SG_stock_price)) + " € (" + str(format(SG_stock_change)) + " %)"
 
 # Vaccinations
-
 url_vaccinations = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile'
 vacc_request = requests.get(url_vaccinations)
 
-with open('Impfquotenmonitoring.xlsx', 'wb') as f:
+datapath = 'Impfquotenmonitoring.xlsx'
+if os.path.exists(datapath):
+  os.remove(datapath)
+
+with open(datapath, 'wb') as f:
     f.write(vacc_request.content)
 
 vaccinations_table = pd.read_excel('Impfquotenmonitoring.xlsx', sheet_name=1)
@@ -98,9 +101,16 @@ vaccination_date = str(vaccinations_date.values.tolist()[1])
 vaccination_date = vaccination_date.split('Impfungen_bis_einschl_', 1)[1]
 vaccination_date = vaccination_date.split(')', 1)[0]
 
-vaccinations_diff = pd.read_excel('~/rpi-rgb-led-matrix/examples-api-use/Impfquotenmonitoring.xlsx', sheet_name=2).values.tolist()
-vaccinations_diff_dates = len(vaccinations_diff)
-vaccinations_diff = (float(vaccinations_diff[vaccinations_diff_dates-1][1]) - float(vaccinations_diff[vaccinations_diff_dates-2][1])) / float(vaccinations_diff[vaccinations_diff_dates-1][1]) * 100
+vaccinations_diff_table = pd.read_excel('Impfquotenmonitoring.xlsx', sheet_name="Impfungen_proTag").fillna("missing").values.tolist()
+vaccinations_diff_dates = len(vaccinations_diff_table)
+
+# fix random appearing numbers in the table
+while vaccinations_diff_table[vaccinations_diff_dates-1][0] == 'missing':
+   vaccinations_diff_dates = vaccinations_diff_dates - 1
+
+vaccinations_diff = (float(vaccinations_diff_table[vaccinations_diff_dates-1][1]) - float(vaccinations_diff_table[vaccinations_diff_dates-2][1])) / float(vaccinations_diff_table[vaccinations_diff_dates-1][1]) * 100
+
+vaccinations_diff_vorz = ""
 if vaccinations_diff > 0:
    vaccinations_diff_vorz = "+"
 
@@ -108,7 +118,7 @@ if vaccinations_diff > 0:
 #vaccination_date_time = vaccinations_date.values.tolist()[1][2]
 #vaccination_date = vaccination_date.strftime("%m.%d.%Y")
 
-vaccinations = "Aktuelle COVID-19-Impfungen in Deutschland: Es wurden " + str("{:,.0f}".format(vaccination_ger_num)) + " Personen (" + str("{:.2f}".format(vaccination_ger_perc)) + " % der Bevölkerung) bis zum " + str(format(vaccination_date)) + " geimpft. An diesem Tag wurden " + str("{:,.0f}".format(vaccination_ger_diff)) + " Personen geimpft (" + vaccinations_diff_vorz + str("{:.2f}".format(vaccinations_diff)) + " % im Vergleich zum Vortag)"
+vaccinations = "Aktuelle COVID-19-Impfungen in Deutschland: Es wurden " + str("{:,.0f}".format(vaccination_ger_num)) + " Personen (" + str("{:.2f}".format(vaccination_ger_perc)) + " % der Bevölkerung) bis zum " + str(format(vaccination_date)) + " geimpft. An diesem Tag wurden " + str("{:,.0f}".format(vaccination_ger_diff)) + " Personen geimpft (" + vaccinations_diff_vorz + str("{:.2f}".format(vaccinations_diff)) + " % im Vergleich zum Vortag)."
 
 # CATFACTS
 
